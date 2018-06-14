a0da9552e3aa6ae586a8eb4e13cf627f98207835
y18aim <y18aim@MacBook-Airyoshi.local>
2018-06-14 20:18:59 +0900

    kekka_uchida

--- countaku_ios/countaku_ios/StoryBoardC/ThirdViewController.swift
+++ countaku_ios/countaku_ios/StoryBoardC/ThirdViewController.swift
@@ -10,26 +10,7 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var Label2: UILabel!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
